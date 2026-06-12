60c1b991630a8bd20afc7bf6b5d2e299fe8532f5
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-06-12 21:15:26 +0700

    Tạo PaymentRepository

---
+++ src/main/java/org/example/datn/Repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package org.example.datn.Repository;
+
+import org.example.datn.Repository.base.BaseRepository;
+import org.example.datn.domain.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends BaseRepository<Payment, Long> {
+
+    Optional<Payment> findByOrderOrderId(Long orderId);
+}
