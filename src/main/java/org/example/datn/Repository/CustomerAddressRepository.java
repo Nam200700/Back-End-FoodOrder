fcf7bf15936a33e918521ce8524bdaaf443185a8
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-06-17 10:17:13 +0700

    Tạo hàm tìm kiếm và set trạng thái của address

--- src/main/java/org/example/datn/Repository/CustomerAddressRepository.java
+++ src/main/java/org/example/datn/Repository/CustomerAddressRepository.java
@@ -2,8 +2,18 @@ package org.example.datn.Repository;
 
 import org.example.datn.Repository.base.BaseRepository;
 import org.example.datn.domain.CustomerAddress;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerAddressRepository extends BaseRepository<CustomerAddress, Long> {
+    List<CustomerAddress> findByCustomerUserId(Long customerId);
+
+    @Modifying
+    @Query("UPDATE CustomerAddress c SET c.isDefault = false WHERE c.customer.userId = :customerId")
+    void resetDefaultAddressByCustomerId(@Param("customerId") Long customerId);
 }
