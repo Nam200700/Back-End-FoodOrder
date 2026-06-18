ab7656079ed36fa4035c1810a4c1c734b3b85b99
Lab08-SOF3041 <nguyentktv00180@gmail.com>
2026-06-18 17:25:13 +0700

    Tạo CreateReviewRequest

--- src/main/java/org/example/datn/DTO/request/review/CreateReviewRequest.java
+++ src/main/java/org/example/datn/DTO/request/review/CreateReviewRequest.java
@@ -1,4 +1,26 @@
-package org.example.datn.DTO.response.review;
+package org.example.datn.DTO.request.review;
 
-public class Createreview {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateReviewRequest {
+
+    @NotNull(message = "orderId không được để trống")
+    private Long orderId;
+
+    @NotNull(message = "Đánh giá quán không được để trống")
+    @Min(value = 1, message = "Đánh giá từ 1 đến 5")
+    @Max(value = 5, message = "Đánh giá từ 1 đến 5")
+    private Integer restaurantRating;
+
+    private String restaurantComment;
+
+    @Min(value = 1, message = "Đánh giá từ 1 đến 5")
+    @Max(value = 5, message = "Đánh giá từ 1 đến 5")
+    private Integer shipperRating;
+
+    private String shipperComment;
 }
