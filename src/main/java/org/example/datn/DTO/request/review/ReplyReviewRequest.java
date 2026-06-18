e11f26d6a142ecb40b91c4ebb7e91c7d85da6340
Lab08-SOF3041 <nguyentktv00180@gmail.com>
2026-06-18 17:27:31 +0700

    Tạo ReplyReviewRequest

--- src/main/java/org/example/datn/DTO/request/review/ReplyReviewRequest.java
+++ src/main/java/org/example/datn/DTO/request/review/ReplyReviewRequest.java
@@ -1,4 +1,11 @@
 package org.example.datn.DTO.request.review;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class ReplyReviewRequest {
+
+    @NotBlank(message = "Nội dung phản hồi không được để trống")
+    private String reply;
 }
