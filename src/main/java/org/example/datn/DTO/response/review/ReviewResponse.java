20c4389b000889c642980eb0347368a8a37b0d47
Lab08-SOF3041 <nguyentktv00180@gmail.com>
2026-06-18 17:20:04 +0700

    Tạo ReviewResponse

--- src/main/java/org/example/datn/DTO/response/review/ReviewResponse.java
+++ src/main/java/org/example/datn/DTO/response/review/ReviewResponse.java
@@ -1,4 +1,24 @@
 package org.example.datn.DTO.response.review;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ReviewResponse {
+    private Long reviewId;
+    private Long orderId;
+    private Long customerId;
+    private String customerName;
+    private Long restaurantId;
+    private Integer restaurantRating;
+    private String restaurantComment;
+    private Integer shipperRating;
+    private String shipperComment;
+    private String merchantReply;
+    private LocalDateTime createdAt;
+    private LocalDateTime repliedAt;
 }
+
