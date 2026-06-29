ed15ed00ed4908ad616d85539b4d5c51de6055e4
unknown <quocanhbuinhat@gmail.com>
2026-06-29 15:21:56 +0700

    add restaurant detail API

--- src/main/java/org/example/datn/Controller/RestaurantController.java
+++ src/main/java/org/example/datn/Controller/RestaurantController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+    
 @RestController
 @RequestMapping("/api/v1/restaurants")
 @RequiredArgsConstructor
