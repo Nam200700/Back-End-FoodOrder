3513befcd1d93c21a1d5b9e29bd66142b8fa13c9
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-07-10 22:13:10 +0700

    Update dto ShippingCalculateResponse.java

--- src/main/java/org/example/datn/DTO/response/shipping/ShippingCalculateResponse.java
+++ src/main/java/org/example/datn/DTO/response/shipping/ShippingCalculateResponse.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @Data
 @Builder
 public class ShippingCalculateResponse {
+    private Long restaurantId;
     private double distanceKm;
+    private double durationMinutes;
     private long shippingFee;
 }
