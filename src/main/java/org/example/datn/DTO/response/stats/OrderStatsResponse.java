e0e4a1cf173360a9cc7a54f37e04834409503830
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-07-30 11:49:45 +0700

    create dto OrderStatsResponse.java

---
+++ src/main/java/org/example/datn/DTO/response/stats/OrderStatsResponse.java
@@ -0,0 +1,17 @@
+package org.example.datn.DTO.response.stats;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class OrderStatsResponse {
+    private long totalOrders;
+    private long completedOrders;
+    private long cancelledOrders;
+    private long processingOrders;
+    private long deliveringOrders;
+    private BigDecimal gmv;
+}
