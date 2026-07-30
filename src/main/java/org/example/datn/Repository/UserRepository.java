2db894d498cd94c6addfba9077e3656ba2dbf337
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-07-30 09:59:12 +0700

    update UserRepository.java: tạo searchUsers

--- src/main/java/org/example/datn/Repository/UserRepository.java
+++ src/main/java/org/example/datn/Repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.example.datn.domain.enums.Role;
 import org.example.datn.Repository.base.BaseRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -26,16 +28,20 @@ public interface UserRepository extends BaseRepository<User, Long> {
     long countByStatusFalse();
 
     long countByRole(Role role);
-
-    // Lọc theo cả Role và Trạng thái Active/Blocked
-    Page<User> findByRoleAndStatus(Role role, Boolean status, Pageable pageable);
-
-    // Chỉ lọc theo Role (lấy tất cả trạng thái)
-    Page<User> findByRole(Role role, Pageable pageable);
-
-    // Chỉ lọc theo Trạng thái Active/Blocked (lấy tất cả role)
-    Page<User> findByStatus(Boolean status, Pageable pageable);
-
     long countByStatusTrue();
     long countByRoleAndStatus(Role role, Boolean status);
+
+    @Query("SELECT u FROM User u WHERE " +
+            "(:keyword IS NULL OR :keyword = '' OR " +
+            "LOWER(u.fullName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(u.phone) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+            "AND (:role IS NULL OR u.role = :role) " +
+            "AND (:status IS NULL OR u.status = :status)")
+    Page<User> searchUsers(
+            @Param("keyword") String keyword,
+            @Param("role") Role role,
+            @Param("status") Boolean status,
+            Pageable pageable
+            );
 }
