fa8b671e680536afd6fea10743a991ee8a2f6a97
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-07-31 16:46:57 +0700

    update VoucherController.java: tạo api get voucher customer

--- src/main/java/org/example/datn/Controller/VoucherController.java
+++ src/main/java/org/example/datn/Controller/VoucherController.java
@@ -3,15 +3,18 @@ package org.example.datn.Controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.datn.DTO.request.voucher.VoucherRequest;
+import org.example.datn.DTO.response.voucher.UserVoucherResponse;
 import org.example.datn.DTO.response.voucher.VoucherResponse;
 import org.example.datn.DTO.response.voucher.VoucherStatsResponse;
 import org.example.datn.Service.VoucherService;
 import org.example.datn.common.ApiResponse;
 import org.example.datn.common.PageResponse;
 import org.example.datn.domain.enums.VoucherStatus;
+import org.example.datn.security.CustomUserDetails;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,12 +22,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/vouchers")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('ADMIN')")
 public class VoucherController {
 
     private final VoucherService voucherService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<PageResponse<VoucherResponse>>> getVouchers(
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false) VoucherStatus status,
@@ -33,23 +36,27 @@ public class VoucherController {
     }
 
     @GetMapping("/stats")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<VoucherStatsResponse>> getStats() {
         return ResponseEntity.ok(ApiResponse.ok(voucherService.getVoucherStats()));
     }
 
     @GetMapping("/{voucherId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<VoucherResponse>> getByIdVoucher(
             @PathVariable Long voucherId) {
         return ResponseEntity.ok(ApiResponse.ok(voucherService.getByIdVoucher(voucherId)));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<VoucherResponse>> createVoucher(
             @Valid @RequestBody VoucherRequest request) {
         return ResponseEntity.ok(ApiResponse.ok(voucherService.createVoucher(request)));
     }
 
     @PutMapping("/{voucherId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<VoucherResponse>> updateVoucher(
             @PathVariable Long voucherId,
             @Valid @RequestBody VoucherRequest request) {
@@ -58,8 +65,18 @@ public class VoucherController {
     }
 
     @PutMapping("/{voucherId}/inactive")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<VoucherResponse>> lockVoucher(
             @PathVariable Long voucherId) {
         return ResponseEntity.ok(ApiResponse.ok(voucherService.lockVoucher(voucherId)));
     }
+
+    @GetMapping("/my-vouchers")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public ResponseEntity<ApiResponse<List<UserVoucherResponse>>> getMyVouchers(
+            @AuthenticationPrincipal CustomUserDetails user) {
+        return ResponseEntity.ok(ApiResponse.ok(voucherService.getMyVouchers(user.getUserId())));
+    }
+
+
 }
