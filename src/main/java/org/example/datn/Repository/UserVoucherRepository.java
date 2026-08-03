a8e2398fdc04fbb4417d51f14b7a717d341638e1
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-08-03 14:44:53 +0700

    update UserVoucherRepository.java: lấy danh sách  voucher còn hạn sử dụng

--- src/main/java/org/example/datn/Repository/UserVoucherRepository.java
+++ src/main/java/org/example/datn/Repository/UserVoucherRepository.java
@@ -2,7 +2,10 @@ package org.example.datn.Repository;
 
 import org.example.datn.Repository.base.BaseRepository;
 import org.example.datn.domain.UserVoucher;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +18,10 @@ public interface UserVoucherRepository extends BaseRepository<UserVoucher, Long>
     boolean existsByUser_UserIdAndVoucher_VoucherId(Long userId, Long voucherId);
 
     Optional<UserVoucher> findByUserUserIdAndVoucherVoucherId(Long userId, Long voucherId);
+
+    @Query("SELECT uv FROM UserVoucher uv WHERE uv.user.userId = :userId AND uv.used = :used AND uv.expiredAt > :currentDate")
+    List<UserVoucher> findValidUserVouchers(@Param("userId") Long userId,
+                                            @Param("used") Boolean used,
+                                            @Param("currentDate") LocalDateTime currentDate);
 
 }
