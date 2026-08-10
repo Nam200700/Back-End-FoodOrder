709ff55347fb8dcadaf77b75ac5326963168e9c9
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-08-10 14:10:25 +0700

    update VoucherResponse.java: thêm minOrderAmount, maxDiscountAmount

--- src/main/java/org/example/datn/DTO/response/voucher/VoucherResponse.java
+++ src/main/java/org/example/datn/DTO/response/voucher/VoucherResponse.java
@@ -19,6 +19,8 @@ public class VoucherResponse {
     private String name;
     private DiscountType discountType;
     private BigDecimal discountValue;
+    private BigDecimal minOrderAmount;
+    private BigDecimal maxDiscountAmount;
     private Integer usedQuantity;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
