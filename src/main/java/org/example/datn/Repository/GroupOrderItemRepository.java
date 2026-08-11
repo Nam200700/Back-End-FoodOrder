010e5e5962a21dab033d1238bd33968bad5a79b9
Trọng Tính <nguyentrongtinh280@gmailcom>
2026-08-11 22:37:31 +0700

    create GroupOrderItemRepository.java

---
+++ src/main/java/org/example/datn/Repository/GroupOrderItemRepository.java
@@ -0,0 +1,20 @@
+package org.example.datn.Repository;
+
+import org.example.datn.Repository.base.BaseRepository;
+import org.example.datn.domain.GroupOrderItem;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GroupOrderItemRepository extends BaseRepository<GroupOrderItem, Long> {
+
+    List<GroupOrderItem> findByGroupOrderGroupOrderId(Long groupOrderId);
+
+    List<GroupOrderItem> findByMemberMemberId(Long memberId);
+
+    Optional<GroupOrderItem> findByGroupOrderItemIdAndMemberMemberId(Long itemId, Long memberId);
+
+    void deleteByMemberMemberId(Long memberId);
+}
